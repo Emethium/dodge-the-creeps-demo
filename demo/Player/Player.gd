8387818468c0194028b4e1d0334eda8dc38db6d7
emethium <nikolas.serafini@gmail.com>
2020-04-20 13:31:06 -0300

    feat(Player, Mob): finished up Player scene, added Mob scene

--- demo/Player/Player.gd
+++ demo/Player/Player.gd
@@ -1,10 +1,18 @@
 extends Area2D
 
+signal hit
+
 export var speed = 400  # How fast the player will move (pixels/sec).
 var screen_size  # Size of the game window.
 
 func _ready():
+	hide()
 	screen_size = get_viewport_rect().size
+
+func start(pos):
+	position = pos
+	show()
+	$CollisionShape2D.disabled = false	
 
 func _process(delta):
 	var velocity = Vector2()  # The player's movement vector.
@@ -29,9 +37,15 @@ func _process(delta):
 	if velocity.x != 0:
 		$AnimatedSprite.animation = "right"
 		$AnimatedSprite.flip_v = false
-		# See the note below about boolean assignment
 		$AnimatedSprite.flip_h = velocity.x < 0
 	elif velocity.y != 0:
 		$AnimatedSprite.animation = "up"
 		$AnimatedSprite.flip_v = velocity.y > 0
+
+
+
+func _on_Player_body_entered(body):
+	hide()  # Player disappears after being hit.
+	emit_signal("hit")
+	$CollisionShape2D.set_deferred("disabled", true)
 
